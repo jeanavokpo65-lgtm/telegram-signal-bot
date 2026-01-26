1dc7dc1613d19bbee00332d71ed2d29d6b2efa58
jeanavokpo65-lgtm <jeanavokpo65@gmail.com>
2026-01-26 21:20:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
 import os
-from telegram.ext import Application, CommandHandler
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
-if not TOKEN:
-    raise ValueError("BOT_TOKEN manquant")
-
 async def start(update, context):
-    await update.message.reply_text("Bot en ligne ✅")
+    await update.message.reply_text("Bot en ligne 🚀")
 
-app = Application.builder().token(TOKEN).build()
+app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 print("Bot démarré...")
 app.run_polling()
+
