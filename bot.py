91e2263b7fd9bd13a686c4d4a8e10d020eb14685
jeanavokpo65-lgtm <jeanavokpo65@gmail.com>
2026-01-27 12:06:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,11 @@ async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(message)
 
+async def chatid(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        f"Chat ID = {update.effective_chat.id}"
+    )
+
 
 def main():
     if not TOKEN:
@@ -35,12 +40,15 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("signal", signal))
+    app.add_handler(CommandHandler("chatid", chatid))
+
 
     print("Bot démarré")
     app.run_polling()
 
 if __name__ == "__main__":
     main()
+
 
 
 
