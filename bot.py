418b8e275685a033166df88a89a3e5cd26863c7c
jeanavokpo65-lgtm <jeanavokpo65@gmail.com>
2026-01-29 11:37:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,26 @@
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
-    CommandHandler,
-)
+from telegram.ext import ApplicationBuilder, ContextTypes
 import os
 
 TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = int(os.environ["CHANNEL_ID"])
+CHAT_ID = "signal24_1"  # ou -100xxxx
 
-async def send_auto_message(context: ContextTypes.DEFAULT_TYPE):
+async def send_message(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=CHAT_ID,
-        text="📢 Message automatique toutes les 3 minutes"
+        text="📢 Message automatique dans le canal"
     )
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Lance le job toutes les 3 minutes
-    context.job_queue.run_repeating(
-        send_auto_message,
-        interval=60,
-        first=0,
-        name="auto_message"
-    )
-    await update.message.reply_text("✅ Envoi automatique démarré")
-
-async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    jobs = context.job_queue.get_jobs_by_name("auto_message")
-    for job in jobs:
-        job.schedule_removal()
-    await update.message.reply_text("⛔ Envoi automatique arrêté")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("stop", stop))
+    app.job_queue.run_repeating(
+        send_message,
+        interval=180,
+        first=0,
+        name="canal_job"
+    )
 
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
